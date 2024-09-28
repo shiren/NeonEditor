0fde029205cc33d0e5a3723123e9590084c70117
Rogier Wiertz <72399947+rawstream@users.noreply.github.com>
2024-09-28 18:53:11 +0200

    fix: don't undo initial state (fix #3266)

--- apps/editor/src/commands/defaultCommands.ts
+++ apps/editor/src/commands/defaultCommands.ts
@@ -7,7 +7,14 @@ export function getDefaultCommands(): Record<string, EditorCommand> {
   return {
     deleteSelection: () => deleteSelection,
     selectAll: () => selectAll,
-    undo: () => undo,
+    undo: () => (state, dispatch) => {
+      const hist = state.history$;
+
+      // The initial state is not counted as a history event
+      if (!hist || hist.done.eventCount === 1) return false;
+      undo(state, dispatch);
+      return true;
+    },
     redo: () => redo,
   };
 }
